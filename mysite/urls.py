f5c1485b135fbd3fc92c5369798e15196e4af2a8
Fox_Ninetales <999tales@gmail.com>
2020-11-18 14:20:01 +0900

    오류 해결

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,14 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
