0267f2e6a72e7c79701b0cf7ed9c313a496674b3
Fox_Ninetales <999tales@gmail.com>
2020-11-18 14:45:17 +0900

    mongo 연결

---
+++ main/templates/board.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# index.html 페이지를 부르는 index 함수
+def index(request):
+    return render(request, 'main/index.html')
+
+
+# blog.html 페이지를 부르는 blog 함수
+def blog(request):
+    return render(request, 'main/blog.html')
