da92f9cd60930f102afd0ecd58194146fcea29b0
moojin4985 <moojin4985@techsoupkorea.kr>
2020-11-18 17:40:36 +0900

    iseeuagain app 삭제
    iseeuagain app 삭제

--- iseeuagain/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IseeuagainConfig(AppConfig):
-    name = 'iseeuagain'
--- iseeuagain/templates/main/uels.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', index),
-    path('blog/', blog),
-]
