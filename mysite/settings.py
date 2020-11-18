bb2650c0868e5ab51863670bd75922db9c562b5e
moojin4985 <moojin4985@techsoupkorea.kr>
2020-11-18 15:46:18 +0900

    home app 추가
    home app 추가 - 사이트 구조상 일단 하나의 app만 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< Updated upstream
     'iseeuagain',
+=======
+    'bootstrap4',
+>>>>>>> Stashed changes
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+<<<<<<< Updated upstream
 from django.urls import path, include
+=======
+from django.urls import include, path
+>>>>>>> Stashed changes
 
 urlpatterns = [
+    path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
