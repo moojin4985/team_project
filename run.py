d1cceb62f14bd95f3a07627b9e0a006783ea6c50
Fox_Ninetales <999tales@gmail.com>
2020-11-18 11:57:43 +0900

    지지인 - 장고 세팅

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
-from flask import flask
+from flask import Flask
+from flask import request
+from flask import render_template
+
 
 app = Flask(__name__)
 
+
+@app.route("/write", methods=["GET", "POST"])
+def board_write():
+    if request.method == "POST":
+        name = request.form.get("name")
+        title = request.form.get("title")
+        contents = request.form.get("contents")
+    else:
+        return render_template("write.html")
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=true, port=9000)
+    app.run(host="0.0.0.0", debug=True, port=9000)
