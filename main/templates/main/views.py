d0563657828a9272a68200256c7545b00381d459
Fox_Ninetales <999tales@gmail.com>
2020-11-18 15:28:25 +0900

    게시판 제작중

--- main/templates/main/views.py
+++ main/templates/main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 
 # index.html 페이지를 부르는 index 함수
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/blog.html')
 
 
-# blog.html 페이지를 부르는 blog 함수
+# list.html 페이지를 부르는 blog 함수
 def blog(request):
     return render(request, 'main/blog.html')
