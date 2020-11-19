000b68609671d0024457cf716175d14d3fd0cc7d
Fox_Ninetales <999tales@gmail.com>
2020-11-19 11:44:12 +0900

    게시판 생성

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Post(models.Model):
+    auther = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    pub_date = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.title
