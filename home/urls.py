e3d08763cb2c82ea822c21afa8df3fe717b2043c
Fox_Ninetales <999tales@gmail.com>
2020-11-20 16:44:30 +0900

    페이지 생성

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('establish', views.establish),
+    path('business', views.business),
+    path('activity', views.activity),
+    path('scontent', views.scontent),
+    path('news', views.news),
+    path('support', views.support),
     path('team', views.team),
-    path('board', views.board)
+    path('event', views.event),
+    path('map', views.map),
+    path('board', views.board),
 ]
--- home/views.py
+++ home/views.py
@@ -8,8 +8,41 @@ from .models import Post
 def home(request):
     return render(request, "base.html")
 
+
+def establish(request):
+    return render(request, "establish.html")
+
+
+def business(request):
+    return render(request, "business.html")
+
+
+def activity(request):
+    return render(request, "activity.html")
+
+
+def scontent(request):
+    return render(request, "scontent.html")
+
+
+def news(request):
+    return render(request, "news.html")
+
+
+def support(request):
+    return render(request, "support.html")
+
+
 def team(request):
     return render(request, "team.html")
+
+
+def event(request):
+    return render(request, "event.html")
+
+
+def map(request):
+    return render(request, "map.html")
 
 
 def board(request):
