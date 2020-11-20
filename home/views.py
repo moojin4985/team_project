6c914688a231afc63e9fcf1baa4984a997aa88f3
Fox_Ninetales <999tales@gmail.com>
2020-11-20 15:39:10 +0900

    몽고디비를 안 거치고 게시글작성

--- home/views.py
+++ home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, loader
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
     return render(request, "home/main.html")
+
+
+def board(request):
+    postlist = Post.objects.all()
+    return render(request, "home/home.html", {'postlist': postlist})
