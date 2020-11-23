d7ce9b19465ab78b60f2aabd0ed8ea29c22073dd
moojin4985 <moojin4985@techsoupkorea.kr>
2020-11-23 23:59:50 +0900

    메뉴바 링크 수정
    메뉴바 링크 수정

--- home/urls.py
+++ home/urls.py
@@ -7,15 +7,15 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('establish', views.establish),
-    path('business', views.business),
-    path('activity', views.activity),
-    path('scontent', views.scontent),
-    path('news', views.news),
-    path('support', views.support),
-    path('team', views.team),
-    path('event', views.event),
-    path('map', views.map),
-    path('board/', views.board),
+    path('establish', views.establish, name='establish'),
+    path('business', views.business, name='business'),
+    path('activity', views.activity, name='activity'),
+    path('scontent', views.scontent, name='scontent'),
+    path('news', views.news, name='news'),
+    path('support', views.support, name='support'),
+    path('team', views.team, name='team'),
+    path('event', views.event, name='event'),
+    path('map', views.map, name='map'),
+    path('board/', views.board, name='board'),
     path('board/<int:pk>/', views.viewboard, name='viewboard'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
