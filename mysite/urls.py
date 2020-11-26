e9c107d177b856f70eac2e7f0ec1d4d7b7e9a7c7
suri <suri@socreative.org>
2020-11-26 22:15:19 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
 
